0b64b4c6004e43ef7169617fb88aa373bcea617e
piotrniezgoda <p.niezgoda@hotmail.com>
2023-02-16 16:39:34 +0100

    GAZ-8539 / add second request

--- src/index.tsx
+++ src/index.tsx
@@ -11,9 +11,9 @@ export function trackEvent({
   tagValue,
 }: TrackEventParams): Promise<any> {
   const url = `https://${dataCenter}.creativecdn.com/tags?type=none&ckt=${cookieType}&ck=${cookie}&id=pr_${hash}_custom_${tagName}_${tagValue}&event_parameters=${multiParamJSON}&v=${appVersion}`
+  const url2 = `https://users-events-lxvi645jpa-lm.a.run.app?type=none&ckt=${cookieType}&ck=${cookie}&id=pr_${hash}_custom_${tagName}_${tagValue}&event_parameters=${multiParamJSON}&v=${appVersion}`
 
-  return new Promise((resolve, reject) => {
-
+  const request1 = new Promise((resolve, reject) => {
     return fetch(url, {
       method: 'POST',
       body: JSON.stringify({})
@@ -25,6 +25,21 @@ export function trackEvent({
       return reject(error);
     });
   });
+
+  const request2 = new Promise((resolve, reject) => {
+    return fetch(url2, {
+      method: 'POST',
+      body: JSON.stringify({})
+    })
+    .then(response => {
+      return resolve(response);
+    })
+    .catch(error => {
+      return reject(error);
+    });
+  });
+
+  return Promise.all([request1, request2])
 }
 
 export type IncrementalitySuiteTrackEventParams = TrackEventParams;
