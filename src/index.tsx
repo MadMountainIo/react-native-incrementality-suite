7d5bfa3f9e73c103bca04e8ab82ac2ef3c281a8a
piotrniezgoda <p.niezgoda@hotmail.com>
2023-02-21 14:34:12 +0100

    GAZ-8539 / request to usersEvents (add condition)

--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,7 @@ export function trackEvent({
   multiParamJSON,
   tagName,
   tagValue,
+  trackOnlyUserEvents
 }: TrackEventParams): Promise<any> {
   const creativeCDNURL = `https://${dataCenter}.creativecdn.com/tags?type=none&ckt=${cookieType}&ck=${cookie}&id=pr_${hash}_custom_${tagName}_${tagValue}&event_parameters=${multiParamJSON}&v=${appVersion}`
   const usersEventsURL = `https://users-events-lxvi645jpa-lm.a.run.app?type=none&ckt=${cookieType}&ck=${cookie}&id=pr_${hash}_custom_${tagName}_${tagValue}&event_parameters=${multiParamJSON}&v=${appVersion}`
@@ -39,7 +40,7 @@ export function trackEvent({
     });
   });
 
-  return Promise.all([creativeCDNRequest, usersEventsRequest])
+  return Promise.all([trackOnlyUserEvents ? '' : creativeCDNRequest, usersEventsRequest])
 }
 
 export type IncrementalitySuiteTrackEventParams = TrackEventParams;
